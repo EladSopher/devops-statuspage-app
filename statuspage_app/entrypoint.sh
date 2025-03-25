8e2802bec2c32b214a8c28c184736042e9e783fb
Elad Sopher <127190903+EladSopher@users.noreply.github.com>
2025-03-26 00:01:35 +0200

    Update entrypoint.sh

--- statuspage_app/entrypoint.sh
+++ statuspage_app/entrypoint.sh
@@ -9,14 +9,14 @@ done
 echo "PostgreSQL is up!"
 
 echo "Running Django migrations..."
-python statuspage/manage.py migrate
+python manage.py migrate
 
 echo "Collecting static files..."
-python statuspage/manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 if [ "$CREATE_SUPERUSER" = "true" ]; then
   echo "Creating superuser if it doesn't exist..."
-  python statuspage/manage.py shell <<EOF
+  python manage.py shell <<EOF
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username='admin').exists():
