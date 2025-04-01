16d50f03f8c31479d2c401ad44e26248a0fa80c8
Elad Sopher <127190903+EladSopher@users.noreply.github.com>
2025-04-01 15:52:47 +0300

    Update configuration.py

--- statuspage_app/statuspage/statuspage/configuration.py
+++ statuspage_app/statuspage/statuspage/configuration.py
@@ -7,7 +7,14 @@ import dj_database_url
 # write access to the server via any other hostnames. The first FQDN in the list will be treated as the preferred name.
 #
 # Example: ALLOWED_HOSTS = ['status-page.example.com', 'status-page.internal.local']
-ALLOWED_HOSTS = ['.imlinfo.xyz', 'app.imlinfo.xyz']
+ALLOWED_HOSTS = [
+    '.imlinfo.xyz',
+    'app.imlinfo.xyz',
+    'statuspage-service',
+    'statuspage-service.statuspage', 
+    'statuspage-service.statuspage.svc', 
+    'statuspage-service.statuspage.svc.cluster.local',
+]
 
 # PostgreSQL database configuration. See the Django documentation for a complete list of available parameters:
 #   https://docs.djangoproject.com/en/stable/ref/settings/#databases
